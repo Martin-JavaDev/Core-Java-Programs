7bf0c96fcc62ad2f5e41d690b51c7947943914e7
martin <enrichmartin2022@gmail.com>
2026-08-13 23:39:57 +0530

    13082026

--- src/FunctionalStyleStringLen.java
+++ src/FunctionalStyleStringLen.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.function.Function;
 
-public class CharacterFrequency {
+public class FunctionalStyleStringLen {
 
     public static void main(String[] args) {
         String name = "MartinRaaj";
